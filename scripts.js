bf172d7b671f80b398098da59094716028ff6f48
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-09-09 00:41:42 -0300

    pages

--- scripts.js
+++ scripts.js
@@ -1,47 +1,110 @@
+
 const userContainer = document.querySelector(".user-list");
+const filter=document.querySelector('filter')
+const next = document.querySelector(".pageN");
+const prev= document.querySelector(".pageP");
+let currentpage = 1;
+const totalPages=42;
 
 // buscar usuários
-async function fetchUsers() {
-  try {
-    const response = await axios.get(
-      "https://rickandmortyapi.com/api/character"
-    );
-    // quando sucesso, o axios retorna os dados no campo data.
-    // essa api tem um array data com os usuários
-    const chars = response.data;
-    const users = chars.results;
-    console.log(users);
 
-    users.forEach((user) => {
-      const userCard = document.createElement("div");
-      userCard.classList.add("card");
-      const cardInfo = document.createElement("div");
-      cardInfo.classList.add("card-info");
-      const separador = document.createElement("hr");
+const fetchUsers = async (page) => {
+  const Response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
+  const data = await Response.json();
+  console.log(data);
+  return data.results;
+};
 
-      userCard.innerHTML = `
+const renderUsers = async (page) => {
+  const data = await fetchUsers(page);
+  
+
+  data.forEach((user) => {
+    const userCard = document.createElement("div");
+    userCard.classList.add("card");
+    const cardInfo = document.createElement("div");
+    cardInfo.classList.add("card-info");
+
+    userCard.innerHTML += `
         <img src="${user.image}" alt="">
        
        
       `;
-      cardInfo.innerHTML = `
+    cardInfo.innerHTML += `
       <h1> ${user.name}</h1>
       <br>
       
       <h3>Status: ${user.status}</h3>
-      
+      <div id='status'>
+      <div class='statusColor ${
+        user.status == 'Dead'
+          ? 'dead'
+          : user.status == 'Alive'
+          ? 'alive'
+          : 'unknown'
+      }'>
+      </div>
+      <span>${user.status}</span>
+      </div>
       <h3>Species: ${user.species}</h3>
       
       <h3> Last known location:</h3>
       <p>${user.location.name}</p>
       
       `;
-      userContainer.appendChild(userCard);
-      userCard.appendChild(cardInfo);
-    });
-  } catch (error) {
-    console.log("Erro ao buscar usuários", error);
-  }
+    userContainer.appendChild(userCard);
+    userCard.appendChild(cardInfo);
+  });
+  console.log(currentpage)
+};
+
+async function main() {
+  const data = await fetchUsers(page)
+ 
+  renderUsers({data})
 }
 
-fetchUsers();
+
+
+next.addEventListener ("click",async () => {
+ if (currentpage <=40) {
+  currentpage+= 1;
+  userContainer.innerHTML=''
+  prev.style.display="block"
+  renderUsers(currentpage)
+  
+ }
+ 
+ 
+else{
+  next.style.display="none"
+
+}
+  
+  
+  });
+
+
+  prev.addEventListener ("click",async () => {
+    if (currentpage >=2) {
+      currentpage-= 1;
+      userContainer.innerHTML=''
+      renderUsers(currentpage)
+      next.style.display="block"
+      
+     }
+    else{
+     
+      prev.style.display="none"
+    }
+    
+    });
+
+
+
+
+renderUsers(currentpage)
+
+
