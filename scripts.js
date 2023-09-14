62cc004601f41c3586e3ada9cafe86a0f5229712
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-09-13 22:51:38 -0300

    identando

--- scripts.js
+++ scripts.js
@@ -1,57 +1,39 @@
-
 const userContainer = document.querySelector(".user-list");
-const filter=document.querySelector('filter')
+const filter = document.querySelector("filter");
 const next = document.querySelector(".pageN");
-const prev= document.querySelector(".pageP");
+const prev = document.querySelector(".pageP");
 let currentpage = 1;
-const totalPages=42;
-let episodeChar=0
-
-
-
+const totalPages = 42;
+let episodeChar = 0;
 
 // buscar usuários
 
-const fetchUsers = async (page,episodes) => {
+const fetchUsers = async (page, episodes) => {
   const Response = await fetch(
     `https://rickandmortyapi.com/api/character/?page=${page}`
   );
-  const episodios=await fetch(episodes)
+  const episodios = await fetch(episodes);
   const data = await Response.json();
   console.log(data);
   return data.results;
 };
 
 const renderUsers = async (page) => {
-
-
   const data = await fetchUsers(page);
-  
-  
- 
 
   data.forEach((user) => {
-   
     const userCard = document.createElement("div");
     userCard.classList.add("card");
     const cardInfo = document.createElement("div");
     cardInfo.classList.add("card-info");
-   
- 
+
     userCard.innerHTML += `
         <img src="${user.image}" alt="">
        
         
       `;
-      
-      switch (user.status) {
-        case "alive":
-          cardInfo.innerHTML +=`<h3>Status:  ${user.status}  <p class="statusColor" ></p> </h3>`
-          break;
-      
-        case "dead":
-          break;
-      }
+
+    
     cardInfo.innerHTML += `
       <h1> ${user.name}</h1>
       <br>
@@ -61,11 +43,11 @@ const renderUsers = async (page) => {
       <div id='status'>
       <span id="spanStatus" >Status: ${user.status}</span>
           <div class='statusColor ${
-           user.status == 'Dead'
-              ? 'dead'
-              : user.status == 'Alive'
-              ? 'alive'
-              : 'unknown'
+            user.status == "Dead"
+              ? "dead"
+              : user.status == "Alive"
+              ? "alive"
+              : "unknown"
           }'>
           </div>
           
@@ -75,72 +57,45 @@ const renderUsers = async (page) => {
       <p>${user.location.name}</p>
       
       `;
-      userCard.innerHTML += `
+    userCard.innerHTML += `
       
      
       
     `;
-     
+
     userContainer.appendChild(userCard);
     userCard.appendChild(cardInfo);
   });
-  console.log(currentpage)
+  console.log(currentpage);
 };
 
 async function main() {
-  const data = await fetchUsers(page)
- 
-  renderUsers({data})
+  const data = await fetchUsers(page);
+
+  renderUsers({ data });
 }
 
+next.addEventListener("click", async () => {
+  if (currentpage <= 40) {
+    currentpage += 1;
+    userContainer.innerHTML = "";
+    prev.style.display = "block";
+    renderUsers(currentpage);
+  } else {
+    next.style.display = "none";
+  }
+});
+
+prev.addEventListener("click", async () => {
+  if (currentpage >= 2) {
+    currentpage -= 1;
+    userContainer.innerHTML = "";
+    renderUsers(currentpage);
+    next.style.display = "block";
+  } else {
+    prev.style.display = "none";
+  }
+});
 
 
-next.addEventListener ("click",async () => {
- if (currentpage <=40) {
-  currentpage+= 1;
-  userContainer.innerHTML=''
-  prev.style.display="block"
-  renderUsers(currentpage)
-  
- }
- 
- 
-else{
-  next.style.display="none"
-
-}
-  
-  
-  });
-
-
-  prev.addEventListener ("click",async () => {
-    if (currentpage >=2) {
-      currentpage-= 1;
-      userContainer.innerHTML=''
-      renderUsers(currentpage)
-      next.style.display="block"
-      
-     }
-    else{
-     
-      prev.style.display="none"
-    }
-    
-    });
-async function lastEpisode(character) {
-	const ep = character.episode.length - 1;
-	try {
-		const response = await fetch(`${character.episode[ep]}`).then((value) =>
-			value.json()
-		);
-		return response.name;
-	} catch (error) {
-		console.log('Erro no Get');
-	}
-}
-
-
-renderUsers(currentpage)
-
-
+renderUsers(currentpage);
