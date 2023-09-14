f68c9cb0b9bd24c425677a11261dd670090e0937
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-09-14 19:21:26 -0300

    puting try catch

--- scripts.js
+++ scripts.js
@@ -9,71 +9,79 @@ let episodeChar = 0;
 // buscar usuários
 
 const fetchUsers = async (page, episodes) => {
-  const Response = await fetch(
+  try { 
+    const Response = await fetch(
     `https://rickandmortyapi.com/api/character/?page=${page}`
   );
   const episodios = await fetch(episodes);
   const data = await Response.json();
   console.log(data);
   return data.results;
+    
+  } catch (error) {
+    console.log(error,"Error");
+    
+  }
+ 
 };
 
 const renderUsers = async (page) => {
-  const data = await fetchUsers(page);
+  try { const data = await fetchUsers(page);
 
-  data.forEach((user) => {
-    const userCard = document.createElement("div");
-    userCard.classList.add("card");
-    const cardInfo = document.createElement("div");
-    cardInfo.classList.add("card-info");
-
-    userCard.innerHTML += `
-        <img src="${user.image}" alt="">
+    data.forEach((user) => {
+      const userCard = document.createElement("div");
+      userCard.classList.add("card");
+      const cardInfo = document.createElement("div");
+      cardInfo.classList.add("card-info");
+  
+      userCard.innerHTML += `
+          <img src="${user.image}" alt="">
+         
+          
+        `;
+  
+      
+      cardInfo.innerHTML += `
+        <h1> ${user.name}</h1>
+        <br>
+        
+        <h3>Species: ${user.species} </h3>
+        
+        <div id='status'>
+        <span id="spanStatus" >Status: ${user.status}</span>
+            <div class='statusColor ${
+              user.status == "Dead"
+                ? "dead"
+                : user.status == "Alive"
+                ? "alive"
+                : "unknown"
+            }'>
+            </div>
+            
+        </div>
+        
+        <h3> Last known location:</h3>
+        <p>${user.location.name}</p>
+        
+        `;
+      userCard.innerHTML += `
+        
        
         
       `;
-
+  
+      userContainer.appendChild(userCard);
+      userCard.appendChild(cardInfo);
+    });
+    console.log(currentpage);
     
-    cardInfo.innerHTML += `
-      <h1> ${user.name}</h1>
-      <br>
-      
-      <h3>Species: ${user.species} </h3>
-      
-      <div id='status'>
-      <span id="spanStatus" >Status: ${user.status}</span>
-          <div class='statusColor ${
-            user.status == "Dead"
-              ? "dead"
-              : user.status == "Alive"
-              ? "alive"
-              : "unknown"
-          }'>
-          </div>
-          
-      </div>
-      
-      <h3> Last known location:</h3>
-      <p>${user.location.name}</p>
-      
-      `;
-    userCard.innerHTML += `
-      
-     
-      
-    `;
-
-    userContainer.appendChild(userCard);
-    userCard.appendChild(cardInfo);
-  });
-  console.log(currentpage);
+  } catch (error) {
+    console.log(error, "render characters error");
+  }
+ 
 };
 
-async function main() {
-  const data = await fetchUsers(page);
 
-  renderUsers({ data });
-}
 
 next.addEventListener("click", async () => {
   if (currentpage <= 40) {
