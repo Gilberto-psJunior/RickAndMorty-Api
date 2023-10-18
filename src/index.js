f7d05441c942261622fe444bab270ca31930cc7e
Gilberto-psJunior <gilbertopsjuniorr@gmail.com>
2023-10-18 18:17:27 -0300

    bootstrapfy

--- src/index.js
+++ src/index.js
@@ -14,75 +14,141 @@ async function fetchRender(page, name = "") {
 
     const users = response.data.results;
     const infos = response.data.info;
+    
 
-    users.forEach((user) => {
-      const userCard = document.createElement("div");
-      userCard.classList.add("card");
-      const cardInfo = document.createElement("div");
-      cardInfo.classList.add("card-info");
+    users.forEach((user,index) => {
+      
+      const lastEpisodeUrl = user.episode[user.episode.length - 1];
+      api
+        .get(lastEpisodeUrl)
+        .then((response) => {
+          const lastEpisode = response.data;
+          const lastEpisodeName = lastEpisode.name;
 
-      userCard.innerHTML += `
-              <img src="${user.image}" alt="">
-             
+    
+      userContainer.innerHTML += `
+
+      <div  class="col pb-5   d-flex justify-content-center ">
+      
+       <a class="col" data-bs-toggle="modal" data-bs-target="#exampleModal-${index}" href="">
+         <div class="card " style="width: 18rem ; padding="">
+         <img src="${user.image}" class="card-img-top" alt="...">
+         
+         
+         <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card-body">
+          <h5 class="fs-3 card-title text-center">${user.name}</h5>
+         
+          <hr>
+          <h5 class="card-text text-center"> Species:${user.species}</h5>
+           <div id='status' class="text-center">
+              <div id="spanStatus" >Status: ${user.status} <div class='statusColor ${
+                user.status == "Dead"
+                  ? "dead"
+                  : user.status == "Alive"
+                  ? "alive"
+                  : "unknown"
+              }'>
+              </div></div>
               
-            `;
-
-      cardInfo.innerHTML += `
-            <h1> ${user.name}</h1>
-            <br>
-            
-            <h3>Species: ${user.species} </h3>
-            
-            <div id='status'>
-            <span id="spanStatus" >Status: ${user.status}</span>
-                <div class='statusColor ${
-                  user.status == "Dead"
-                    ? "dead"
-                    : user.status == "Alive"
-                    ? "alive"
-                    : "unknown"
-                }'>
-                </div>
-                
-            </div>
-            
-            <h3> Last known location:</h3>
-            <p>${user.location.name}</p>
-            
-            `;
-      userCard.innerHTML += `
-            
-           
-            
-          `;
-
-      userContainer.appendChild(userCard);
-      userCard.appendChild(cardInfo);
-    });
+              
+              </div>
+         
+         
+              </div>
+          
+               </div>
+             </div>
+       </a>
+    </div>
+    <div class="modal mods" id="exampleModal-${index}" tabindex="-1">
+  <div class="modal-dialog mods d-flex justify-content-center">
+    <div class="modal-content d-flex justify-content-center">
+      
+        <h5  class=" fs-2 text-center" >${user.name}</h5>
+        <img src="${user.image}" class="card-img-top" alt="...">
+      
+      <div class="modal-body">
+      <h5 id="spanStatus" class="card-text text-center fs-3 "> Species: ${user.species}</h5>
+      <div id='status' class="text-center ">
+         <div id="spanStatus" >Status: ${user.status} <div class=' statusColor ${
+           user.status == "Dead"
+             ? "dead"
+             : user.status == "Alive"
+             ? "alive"
+             : "unknown"
+         }'>
+         </div></div>
+         <h5 id="spanStatus" class="card-text"> Last known location:</h5>
+         <p>${user.location.name}</p>
+         <p id="spanStatus">Last seen in episode: </p><p>${lastEpisodeName}</p>
+         </div>
+      </div>
+      <div class="modal-footer d-flex justify-content-center ">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    
+         `;
+         
+        })
+        .catch((error) => console.log(error)); });
+   
     console.log(currentpage);
   } catch (error) {
     console.log(error, "render characters error");
   }
 }
 
+
+
+async function searchLastEp(urlEpisodio) {
+  try {
+    const response = await axios.get(urlEpisodio);
+    const ultimoEpisodio = response.data;
+    console.log("ultimoEpisodio.name");
+    return ultimoEpisodio.name;
+  } catch (error) {
+    console.error("Error getting last episode details:", error);
+    return "Error getting episode details";
+  }
+}
 searchCharacter.addEventListener("input", (event) => {
   fetchRender(currentpage, searchCharacter.value);
   userContainer.innerHTML = "";
 });
 
 next.addEventListener("click", () => {
-  if (currentpage < 42) {
+  if (currentpage <=41) {
     userContainer.innerHTML = "";
     currentpage += 1;
+    prev.style.display='block';
+   
+    
     fetchRender(currentpage,searchCharacter.value);
   }
+  
+ 
+ 
 });
+
 prev.addEventListener("click", () => {
   if (currentpage > 1) {
+    
     userContainer.innerHTML = "";
     currentpage -= 1;
+    
     fetchRender(currentpage,searchCharacter.value);
   }
 });
+
+function hide() {
+  if (currentpage > 1) {
+    prev.style.display = "none";
+    next.style.display = "block";
+  }
+}
 
 fetchRender();
